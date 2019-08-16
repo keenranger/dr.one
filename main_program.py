bbecf6effdfe3afe31e968f740b65f0612e440b1
keenranger <kghnkl0103@gmail.com>
2019-08-16 16:41:57 +0100

    attitude added to main

--- main_program.py
+++ main_program.py
@@ -1,10 +1,21 @@
-from modules import drone_server
+from modules import drone_server, drone_msp
 from threading import Thread, Lock
 import time
 if __name__ == '__main__':
-    t = Thread(target=drone_server.run_server)
+    t = Thread(target=drone_server.run_server) #run drone server
     t.start()
-    while True:
+
+    board = drone_msp.MultiWii("/dev/ttyACM0")
+    try:
+        while True:
+            board.getData(drone_msp.MultiWii.ATTITUDE)
+            message = ("angx = {:+.2f} \t angy = {:+.2f} \t \
+                        heading = {:+.2f} \t \elapsed = {:+.4f} \t"\
+                        .format(float(board.attitude['angx']),float(board.attitude['angy']),\
+                            float(board.attitude['heading']),float(board.attitude['elapsed'])))
             print(drone_server.command)
             print(drone_server.value)
-            time.sleep(1)
+            print(message)
+            time.sleep(0.1)
+    except Exception. error:
+        print ("Error on Main: "+str(error))
