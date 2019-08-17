183fc113232ffc62bd92c0048ef7372fe726acfa
HanKyeol Kyung <kghnkl0103@gmail.com>
2019-08-17 09:31:50 +0900

    command transmission update

--- main_program.py
+++ main_program.py
@@ -8,17 +8,24 @@ if __name__ == '__main__':
     board = drone_msp.MultiWii("/dev/ttyACM0")
     try:
         while True:
-            board.getData(drone_msp.MultiWii.ATTITUDE)
-            #board.sendCMDreceiveATT(self, data_length, code, data)
-            message = ("angx = {:+.2f} \t angy = {:+.2f} \t \
+            action = [1500, 1500, 1500, 800, 1500, 1500, 1500, 1500] #default value
+            #board.getData(drone_msp.MultiWii.ATTITUDE)
+            board.sendCMDreceiveATT(16, drone_msp.MultiWii.SET_RAW_RC, action)
+            '''message = ("angx = {:+.2f} \t angy = {:+.2f} \t \
                         heading = {:+.2f} \t \elapsed = {:+.4f} \t"\
                         .format(float(board.attitude['angx']),float(board.attitude['angy']),\
-                            float(board.attitude['heading']),float(board.attitude['elapsed'])))
+                            float(board.attitude['heading']),float(board.attitude['elapsed'])))'''
             print(drone_server.command)
             print(drone_server.value)
-            action=drone_calculation(drone_server.command, drone_server.value)
+            if (drone_server.command=='A'):
+                board.arm()
+            elif (drone_server.command=='U'):
+                
+            elif (drone_server.command=='D'):
+            else:
+                action=drone_calculation(drone_server.command, drone_server.value)
             print(action)
-            print(message)
+            #print(message) #for debug
             time.sleep(0.1)
     except Exception. error:
         print ("Error on Main: "+str(error))
--- modules/drone_calculation.py
+++ modules/drone_calculation.py
@@ -1,16 +1,16 @@
 # hello
 def command_to_action(command, value):
-    action = {'roll' = 1500, 'pitch' = 1500 'yaw' = 1500, 'throttle' = 1500}
+    action = [1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500]
     if (command == 'S'): #stop = hovering
     elif (command == 'F'): #Forward
-        action['pitch'] += float(value*5)
+        action[1] += float(value*5)
     elif (command == 'B'): #Backward
-        action['pitch'] -= float(value*5)
+        action[1] -= float(value*5)
     elif (command == 'L'): #Left
-        action['roll'] -= float(value*5)
+        action[0] -= float(value*5)
     elif (command == 'R'): #Right
-        action['roll'] += float(value*5)
-    elif (command == 'A'): #Arming
-        action['yaw'] = 2000
-        action['throttle'] = 1000
+        action[0] += float(value*5)
+    #elif (command == 'A'): #Arming
+    #    action['yaw'] = 2000
+    #    action['throttle'] = 1000
     return action
