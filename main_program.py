fcaee280345c7b1b914d8a0dad3971dbeb4fbb9e
HanKyeol Kyung <kghnkl0103@gmail.com>
2019-08-22 16:14:33 +0900

    code revival, maybe possible with ppm

--- main_program.py
+++ main_program.py
@@ -10,19 +10,16 @@ if __name__ == '__main__':
     t = Thread(target=drone_server.run_server) #run drone server
     t.start()
     board = drone_msp.MultiWii("/dev/ttyACM0") #connect via usb
-    #board = drone_msp.MultiWii("/dev/ttyAMA0") #connect via tx/rx
     try:
         while True:
             action = [1500, 1500, 1500, 800] #default value
             
             board.getData(drone_msp.MultiWii.ATTITUDE)
-            board.sendCMDreceiveATT(8, drone_msp.MultiWii.SET_RAW_RC, action)
             message = ("angx = {:+.2f} \t angy = {:+.2f} \t heading = {:+.2f} \t \elapsed = {:+.4f} \t"\
                         .format(float(board.attitude['angx']),float(board.attitude['angy']),\
                             float(board.attitude['heading']),float(board.attitude['elapsed'])))
             print(drone_server.command)
             if (drone_server.command=='A'):
-                board.arm()
                 print('arm done')
                 drone_server.command='S'
             elif (drone_server.command=='U'):
@@ -30,7 +27,6 @@ if __name__ == '__main__':
             elif (drone_server.command=='D'):
                 pass
             else:
-                action=drone_calculation.command_to_action(drone_server.command, drone_server.value)
                 pass   
             print(action)
             print(message) #for debug
