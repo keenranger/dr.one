928df51d86be8a9ce709ca52f996c546a01fed3e
HanKyeol Kyung <kghnkl0103@gmail.com>
2019-08-26 21:47:32 +0900

    done, need test

--- main_program.py
+++ main_program.py
@@ -6,6 +6,7 @@ import smbus
 if __name__ == '__main__':
     arduino_addr = 0x04
     I2Cbus = smbus.SMBus(1)
+    pid = drone_calculation.PID()
 
     t = Thread(target=drone_server.run_server) #run drone server
     t.start()
@@ -13,33 +14,29 @@ if __name__ == '__main__':
     board = drone_msp.MultiWii("/dev/ttyACM0") #connect via usb
     try:
         while True:
-            ppm_command = [1500, 1500, 1500, 800] #default value
+            ppm_command = [1500, 1500, 1500, 1000] #default value
             bldc=[0,0]
             board.getData(drone_msp.MultiWii.ATTITUDE)
-            attitude=[(float(board.attitude['angx']),(float(board.attitude['angy']),(float(board.attitude['heading'])]
+            attitude=[float(board.attitude['angx']), float(board.attitude['angy']), float(board.attitude['heading'])]
             print(attitude)
             '''message = ("angx = {:+.2f} \t angy = {:+.2f} \t heading = {:+.2f} \t \elapsed = {:+.4f} \t"\
                         .format(float(board.attitude['angx']),float(board.attitude['angy']),\
                             float(board.attitude['heading']),float(board.attitude['elapsed'])))'''
             print(drone_server.command)
-            print(attitude)
-            ppm_command=command_to_ppm_command(drone_server.command, drone_server.value)
+            ppm_command=drone_calculation.command_to_ppm_command(drone_server.command, drone_server.value)
 
             if (drone_server.command=='A'):
                 print('arm done')
                 drone_server.command='S'
-            elif (drone_server.command=='U'):
-                pass
-            elif (drone_server.command=='D'):
-                pass
-            else:
-                pass   
-                
+            
+            bldc=pid.calculate_bldc(ppm_command[2],ppm_command[3], attitude[2]) #yaw, throttle, heading
             for ppm_block in ppm_command:
-                I2Cbus.write_i2c_block_data(arduino_addr,"/",ppm_block)
+                block=str(ppm_block)
+                I2Cbus.write_i2c_block_data(arduino_addr, 47, [int(block[0]), int(block[1]), int(block[2]), int(block[3])])
             for bldc_block in bldc:
-                I2Cbus.write_i2c_block_data(arduino_addr,"/",bldc_block)
-            I2Cbus.write_byte_data(arduino_addr, "/")
+                block=str(bldc_block+1000)#for box matching +1000
+                I2Cbus.write_i2c_block_data(arduino_addr, 47, [int(block[0]), int(block[1]), int(block[2]), int(block[3])]) 
+            I2Cbus.write_byte_data(arduino_addr, 47)
             time.sleep(0.3)
     except Exception as error:
         print ("Error on Main: "+str(error))
--- modules/drone_calculation.py
+++ modules/drone_calculation.py
@@ -1,4 +1,3 @@
-# hello
 def command_to_ppm_command(command, value):
     ppm_command = [1500, 1500, 1500, 1500]
     if (command == 'S'): #stop = hovering
@@ -17,8 +16,56 @@ def command_to_ppm_command(command, value):
         action[3] -= int(value*5)
     elif (command == 'D'): #Down
         action[3] += int(value*5)
-        
+
     elif (command == 'A'): #Arming
         action[2] = 2000 #yaw
         action[3] = 1000 #throttle
     return ppm_command
+
+class PID:
+    def __init__(self):
+        self.diffAngle=0
+        self.angle=0
+        self.preAngle=0
+        self.control=0
+        self.kp=1
+        self.ki=0.1
+        self.kd=0
+        self.p=0
+        self.i=0
+        self.d=0
+        self.err=0
+        self.err_prev=0
+        self.de=0
+        self.dt=0
+        self.final=0
+    def calculate_bldc(self, yaw, throttle, heading):
+        throttle-=1000
+        self.control = (yaw-1500)*45/500 +10
+        self.angle=heading
+        self.diffAngle=self.angle-self.preAngle
+        self.preAngle=self.angle
+
+        self.err=self.control-self.diffAngle
+        self.dt=0.001
+        self.p = self.err * self.kp
+        self.i = self.i + self.err * self.dt * self.ki
+        self.i = constrain(self.i, -10, 10)
+        self.final = self.p + self.i
+        self.final = constrain(self.final, -10, 10)
+        if (self.final>0):
+            bl0_setValue = -abs(self.final) / 2.
+            bl1_setValue = +abs(self.final) / 2.
+        else:
+            bl0_setValue = +abs(self.final) / 2.
+            bl1_setValue = -abs(self.final) / 2.
+        bl0_setValue +=throttle
+        bl1_setValue +=throttle
+        bl0_setValue = constrain(bl0_setValue, 0, 1000)
+        bl1_setValue = constrain(bl1_setValue, 0, 1000)
+        bl0=int(bl0_setValue/10)
+        bl1=int(bl1_setValue/10)
+        return [bl0, bl1]
+        
+def constrain(val, min_val, max_val):
+    return min(max_val, max(min_val, val))
